661d0e618a1b34ce1052cb94e4fe2cb814471e6f
gabrivava <gabriele.vavassori4@gmail.com>
2021-06-28 15:03:03 +0200

    extend Product and User to products and userPremium

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ $user->insertCreditCard($c); */
 
 # classi generali ( PRODOTTI / CLIENTI )
 
+// Prodotti
 class Product
 {
     public $name;
@@ -27,23 +28,63 @@ class Product
 # extend a categorie minori
 class Cloth extends Product 
 {
+    public function __construct($nome, $desc, $prezzo, $quant, $size, $color) {
 
+        parent:: __construct($nome, $desc, $prezzo, $quant);
+        $this->size = $size;
+        $this->color = $color;
+
+    }
 }
 class Car extends Product
 {
+    public function __construct($nome, $desc, $prezzo, $quant, $engine, $fuel) {
 
+        parent:: __construct($nome, $desc, $prezzo, $quant);
+        $this->engine = $engine;
+        $this->fuel = $fuel;
+
+    }
 }
 class Smartphone extends Product
 {
+    public function __construct($nome, $desc, $prezzo, $quant, $memory, $facescan) {
 
+        parent:: __construct($nome, $desc, $prezzo, $quant);
+        $this->memory = $memory;
+        $this->facescan = $facescan;
+
+    }
 }
+
+// Utenti
 class User
 {
     public $name;
     public $username;
     protected $mail;
     protected $password;
+
+    public function __construct(string $name,string $username,string $mail,string $password) {
+        $this->name = $name;
+        $this->username = $username;
+        $this->mail = $mail;
+        $this->password = $password;
+    }
 }
+class UserPremium extends User 
+{
+    public function __construct($name, $username, $mail, $password, $scontoPremium, $puntiPremium) {
+
+        parent:: __construct($name, $username, $mail, $password);
+        $this->scontoPremium = $scontoPremium;
+        $this->puntiPremium = $puntiPremium;
+    }
+}
+
 
 $contoller = new Product('Elite', 'Xbox', 99.99, 'lorem controller');
 var_dump($contoller);
+
+$io = new User('Gabriele', 'Vava98', 'mail.mail@gmial.com', '123457AB');
+var_dump($io);
